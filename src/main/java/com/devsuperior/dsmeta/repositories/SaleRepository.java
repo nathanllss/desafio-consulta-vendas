3d1ead7e3b1025c14e8612642d0f17059070453c
Nathan <nathan.llucasl@gmail.com>
2025-01-07 11:18:34 -0300

    fix: countQuery params

--- src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
+++ src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
@@ -10,7 +10,7 @@ import java.time.LocalDate;
 
 public interface SaleRepository extends JpaRepository<Sale, Long> {
 
-    @Query(value = "SELECT obj FROM Sale obj JOIN FETCH obj.seller WHERE obj.date BETWEEN :minDate and :maxDate and UPPER(obj.seller.name) LIKE UPPER(CONCAT('%',:name,'%')) ",
-            countQuery = "SELECT COUNT(obj) FROM Sale obj JOIN obj.seller ")
+    @Query(value = "SELECT obj FROM Sale obj JOIN FETCH obj.seller WHERE obj.date BETWEEN :minDate AND :maxDate AND UPPER(obj.seller.name) LIKE UPPER(CONCAT('%', :name, '%'))",
+            countQuery = "SELECT COUNT(obj) FROM Sale obj JOIN obj.seller WHERE obj.date BETWEEN :minDate AND :maxDate AND UPPER(obj.seller.name) LIKE UPPER(CONCAT('%', :name, '%'))")
     Page<Sale> findReport(LocalDate minDate, LocalDate maxDate, String name, Pageable pageable);
 }
